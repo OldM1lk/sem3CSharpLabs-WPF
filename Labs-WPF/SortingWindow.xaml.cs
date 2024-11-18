52c81986495cb313d8e53e4d835dad5637fb8420
OldM1lk <MrlaimWin@gmail.com>
2024-11-18 19:41:55 +0700

    Add sorting window and interface for it (doesn't work at all)

---
+++ Labs-WPF/SortingWindow.xaml.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.Win32;
+using System.Windows;
+
+namespace Labs_WPF
+{
+    /// <summary>
+    /// Логика взаимодействия для SortingWindow.xaml
+    /// </summary>
+    public partial class SortingWindow : Window
+    {
+        public SortingWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void bogoSortCB_Click(object sender, RoutedEventArgs e)
+        {
+            if (bogoSortCB.IsChecked == true)
+            {
+                bogoSortIterationsTB.IsEnabled = true;
+            }
+            else
+            {
+                bogoSortIterationsTB.IsEnabled = false;
+            }
+        }
+
+        private void manualInputRB_Checked(object sender, RoutedEventArgs e)
+        {
+            if (manualInputRB.IsChecked == true)
+            {
+                manualInputBtn.IsEnabled = true;
+                generateElementsNumberTB.IsEnabled = false;
+                minNumberTB.IsEnabled = false;
+                maxNumberTB.IsEnabled = false;
+                chooseFileBtn.IsEnabled = false;
+            }
+        }
+
+        private void generateRB_Click(object sender, RoutedEventArgs e)
+        {
+            if (generateRB.IsChecked == true)
+            {
+                generateElementsNumberTB.IsEnabled = true;
+                manualInputBtn.IsEnabled = false;
+                minNumberTB.IsEnabled = true;
+                maxNumberTB.IsEnabled = true;
+                chooseFileBtn.IsEnabled = false;
+            }
+        }
+
+        private void fileInputRB_Click(object sender, RoutedEventArgs e)
+        {
+            if (fileInputRB.IsChecked == true)
+            {
+                chooseFileBtn.IsEnabled = true;
+                manualInputBtn.IsEnabled = false;
+                generateElementsNumberTB.IsEnabled = false;
+                minNumberTB.IsEnabled = false;
+                maxNumberTB.IsEnabled = false;
+            }
+        }
+
+        private void chooseFileBtn_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "(*.txt, *.xlsx)|*.txt;*xlsx";
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                string filePath = openFileDialog.FileName;
+            }
+        }
+    }
+}
